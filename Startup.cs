562be6bee69d8dba3530fb13a72dc9ec9af4a383
Niklas <36010519+neon-JS@users.noreply.github.com>
2021-01-26 21:29:58 +0100

    chore: Update to .NET Core 5
    - Update .NET
    - Stop listening on all interfaces

--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Hosting;
 
 namespace HTTPSMiddleware
 {
@@ -25,11 +18,11 @@ namespace HTTPSMiddleware
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -41,7 +34,8 @@ namespace HTTPSMiddleware
                 app.UseHsts();
             }
 
-            app.UseMvc();
+            app.UseRouting();
+            app.UseEndpoints(e => e.MapControllers());
         }
     }
 }
